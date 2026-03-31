a9ede93d354649da069b54b83b37aaf4bcd3b2a3
Alex Jongman <alex.jongman@hu.nl>
2026-03-31 22:13:30 +0000

    Enhance navigation and rendering functionality; update button states and image display based on map data

--- src/view/location.js
+++ src/view/location.js
@@ -31,11 +31,37 @@ export class Location {
     }
 
     render() {
+        // Debugging info
         console.log(`Rendering location at (${this.currentPosition.x}, ${this.currentPosition.y}) with map data:`, this.mapData);
         console.log(`Mapdata type: ${typeof this.mapData}, keys: ${Object.keys(this.mapData)}`);
         console.log(`Mapdata metadata:`, this.mapData.metadata);
 
+        // Update the Title
+        const titleElement = document.querySelector('.location-title');
+        titleElement.textContent = `${this.mapData.metadata.name} (${this.currentPosition.x}, ${this.currentPosition.y})`;
+
+        // Update the Image
         const imgElement = document.querySelector('#location-image');
         imgElement.setAttribute('src', `./images/${this.mapData.metadata.photo}`);
+        imgElement.setAttribute('alt', `Location at (${this.currentPosition.x}, ${this.currentPosition.y})`);
+
+        // Enable/Disable Navigation Buttons
+        const northWestButton = document.querySelector('#north-west');
+        const northButton = document.querySelector('#north');
+        const northEastButton = document.querySelector('#north-east');
+        const westButton = document.querySelector('#west');
+        const eastButton = document.querySelector('#east');
+        const southWestButton = document.querySelector('#south-west');
+        const southButton = document.querySelector('#south');
+        const southEastButton = document.querySelector('#south-east');
+
+        northWestButton.disabled = !this.mapData.deelmatrix[0][0];
+        northButton.disabled = !this.mapData.deelmatrix[0][1];
+        northEastButton.disabled = !this.mapData.deelmatrix[0][2];
+        westButton.disabled = !this.mapData.deelmatrix[1][0];
+        eastButton.disabled = !this.mapData.deelmatrix[1][2];
+        southWestButton.disabled = !this.mapData.deelmatrix[2][0];
+        southButton.disabled = !this.mapData.deelmatrix[2][1];
+        southEastButton.disabled = !this.mapData.deelmatrix[2][2];
     }
 }
